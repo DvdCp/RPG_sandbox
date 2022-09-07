5f8720d27c0ddc6e3641ad42ce6d08b7290a142e
DavidC <dav.capuano98@gmail.com>
2022-09-07 23:09:05 +0200

    Adding coroutines to portals

--- Assets/Scripts/Core/Portal.cs
+++ Assets/Scripts/Core/Portal.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +6,47 @@ namespace RPG.Core
 { 
     public class Portal : MonoBehaviour
     {
+        [SerializeField] int sceneToLoad = 1;
+        [SerializeField] Transform spawnPoint;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
-                SceneManager.LoadScene(1);
+                StartCoroutine(Transition(other.transform));
+                           
+        }
+
+        private IEnumerator Transition(Transform player)
+        {
+            // Preventing destroy portal when the new scene is loading
+            //DontDestroyOnLoad(gameObject);
+            // P.N: this method doesn't work as we want in Unity 2021.3.3f1
+            
+            // Loading new scene in background
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            // Finding portal into new loaded scene
+            Portal arrivingPortal = getNextPortal();
+
+            // Updating player location
+            player.position = arrivingPortal.spawnPoint.transform.position;
+            
+            // Now destroy this portal
+            Destroy(gameObject);
+
+        }
+
+        private Portal getNextPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            { 
+                if (portal == this) continue;
+
+                return portal;            
+            }
+
+            // if no portals are found
+            return null;
         }
     }
 
